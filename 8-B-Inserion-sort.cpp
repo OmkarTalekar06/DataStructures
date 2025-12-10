3c7e89a4a2e5381832c739f4ede1cca94b156645
OmkarTalekar06 <sanskrutitalekar1982@gmail.com>
2025-12-10 19:49:48 +0530

    10-12-25

---
+++ 8-B-Inserion-sort.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n){
+    for (int i = 0; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i-1;
+        while (j>=0 && arr[j] > key)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+}
+
+void printArray(int arr[], int n){
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printArray(arr, n);
+    insertionSort(arr, n);
+    cout<<"Insertion Array : " ;
+    printArray(arr, n);
+
+    return 0;   
+}
